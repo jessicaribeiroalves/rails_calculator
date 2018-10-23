235219a0732417796b7b9873bdf593a6c2a1ca34
Jessica Alves <jessralves@gmail.com>
2018-10-23 19:46:11 +1100

    Implement calculator controller, add .DS_Store to gitignore

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,12 +1,31 @@
 class CalculatorController < ApplicationController
     def index
-  
+
     end
-    def calculate        
-      # Your code here
+    def calculate
+      # render json: params
+      @operation = params[:operation]
+      @x = Float(params[:x]) rescue false
+      @y = Float(params[:y]) rescue false
+      
+      if @x != false && @y != false
 
+        if @operation == "add" && @x ==
+          @result = @x.to_f + @y.to_f
+        elsif @operation == "subtract"
+          @result = @x.to_f - @y.to_f
+        elsif @operation == "multiply"
+          @result = (@x.to_f) * (@y.to_f)
+        elsif @operation == "divide"
+          @result = (@x.to_f) / (@y.to_f)
+        end
 
+      else
+        @result = "ERROR"
+      end
+  
       render "index"
     end
+
   end
   
